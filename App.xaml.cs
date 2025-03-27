9ab85e6928a34d36ab05643e002c03fa9e1ae3e5
Voushikha <149755814+Voushikha@users.noreply.github.com>
2025-03-27 10:47:05 +0800

    frontend requirements

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AT1_Sensor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
